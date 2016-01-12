b48ad657f116fd4d3eed514f9324b60290a9acfa
Tony Bowden <tony@tmtm.com>
2016-01-12 17:13:50 +0000

    Modernise scraper

--- scraper.rb
+++ scraper.rb
@@ -1,43 +1,21 @@
 #!/bin/env ruby
 # encoding: utf-8
 
-require 'scraperwiki'
 require 'wikidata/fetcher'
-require 'nokogiri'
-require 'colorize'
 require 'pry'
-require 'rest-client'
-require 'open-uri/cached'
-OpenURI::Cache.cache_path = '.cache'
 
 
-def noko_for(url)
-  Nokogiri::HTML(open(URI.escape(URI.unescape(url))).read) 
-end
+parl42 = EveryPolitician::Wikidata.wikipedia_xpath( 
+  url: 'https://en.wikipedia.org/wiki/List_of_House_members_of_the_42nd_Parliament_of_Canada',
+  xpath: '//table[.//tr[th[.="Electoral district"]]]//tr[td]//td[2]//a[not(@class="new")]/@title',
+) 
 
-def wikinames_from(url)
-  noko = noko_for(url)
-  names = noko.xpath('//table[.//tr[th[.="Electoral district"]]]//tr[td]//td[2]//a[not(@class="new")]/@title').map(&:text) 
-  abort "No names" if names.count.zero?
-  names
-end
+parl41 = EveryPolitician::Wikidata.wikipedia_xpath( 
+  url: 'https://en.wikipedia.org/wiki/List_of_House_members_of_the_41st_Parliament_of_Canada',
+  xpath: '//table[.//tr[th[.="Electoral district"]]]//tr[td]//td[2]//a[not(@class="new")]/@title',
+) 
 
-def fetch_info(names)
-  WikiData.ids_from_pages('en', names).each do |name, id|
-    data = WikiData::Fetcher.new(id: id).data('en', 'fr') rescue nil
-    unless data
-      warn "No data for #{p}"
-      next
-    end
-    data[:original_wikiname] = name
-    ScraperWiki.save_sqlite([:id], data)
-  end
-end
-
-names = wikinames_from('https://en.wikipedia.org/wiki/List_of_House_members_of_the_42nd_Parliament_of_Canada') +
-  wikinames_from('https://en.wikipedia.org/wiki/List_of_House_members_of_the_41st_Parliament_of_Canada')
-
-fetch_info names.uniq
-
-warn RestClient.post ENV['MORPH_REBUILDER_URL'], {} if ENV['MORPH_REBUILDER_URL']
+names = (parl41 + parl42).uniq
+EveryPolitician::Wikidata.scrape_wikidata(names: { en: names })
+warn EveryPolitician::Wikidata.notify_rebuilder
 
