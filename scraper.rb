ee90ea25826517d17f4441429a9891fba023c799
Tony Bowden <tony@tmtm.com>
2018-03-07 07:49:16 +0000

    Also fetch members with correct P39s

--- scraper.rb
+++ scraper.rb
@@ -4,16 +4,28 @@
 require 'wikidata/fetcher'
 require 'pry'
 
-
-parl42 = EveryPolitician::Wikidata.wikipedia_xpath( 
+parl42 = EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://en.wikipedia.org/wiki/List_of_House_members_of_the_42nd_Parliament_of_Canada',
   xpath: '//table[.//tr[th[.="Electoral district"]]]//tr[td]//td[2]//a[not(@class="new")]/@title',
-) 
+)
 
-parl41 = EveryPolitician::Wikidata.wikipedia_xpath( 
+parl41 = EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://en.wikipedia.org/wiki/List_of_House_members_of_the_41st_Parliament_of_Canada',
   xpath: '//table[.//tr[th[.="Electoral district"]]]//tr[td]//td[2]//a[not(@class="new")]/@title',
-) 
+)
 
-EveryPolitician::Wikidata.scrape_wikidata(names: { en: parl41 | parl42 })
+# Find all members of (terms that started after 2011)
+query = <<EOS
+  SELECT DISTINCT ?item
+  WHERE
+  {
+    ?item p:P39 ?st .
+    ?st ps:P39 wd:Q15964890 ; pq:P2937 ?term .
+    ?term wdt:P571|wdt:P580 ?start
+    FILTER (?start >= "2011-01-01T00:00:00Z"^^xsd:dateTime) .
+  }
+EOS
+p39s = EveryPolitician::Wikidata.sparql(query)
+
+EveryPolitician::Wikidata.scrape_wikidata(ids: p39s, names: { en: parl41 | parl42 })
 
