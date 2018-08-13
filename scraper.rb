2c7e1362f2f89f4a495aa907e31b03af332e5408
Tony Bowden <tony@tmtm.com>
2018-08-13 12:00:28 +0100

    Look for columns _containing_ header
    The columns now have a sort button, meaning the text isn't exactly
    *equal* to the required phrase now, but merely contains it.

--- scraper.rb
+++ scraper.rb
@@ -6,12 +6,12 @@ require 'pry'
 
 parl42 = EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://en.wikipedia.org/wiki/List_of_House_members_of_the_42nd_Parliament_of_Canada',
-  xpath: '//table[.//tr[th[.="Electoral district"]]]//tr[td]//td[2]//a[not(@class="new")]/@title',
+  xpath: '//table[.//tr[th[contains(.,"Electoral district")]]]//tr[td]//td[2]//a[not(@class="new")]/@title',
 )
 
 parl41 = EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://en.wikipedia.org/wiki/List_of_House_members_of_the_41st_Parliament_of_Canada',
-  xpath: '//table[.//tr[th[.="Electoral district"]]]//tr[td]//td[2]//a[not(@class="new")]/@title',
+  xpath: '//table[.//tr[th[contains(.,"Electoral district")]]]//tr[td]//td[2]//a[not(@class="new")]/@title',
 )
 
 # Find all members of (terms that started after 2011)
